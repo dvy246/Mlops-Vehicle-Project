76459248531b2429d5d26dc56e5194e3261f8a88
Divy <bhawnayadav@Bhawnas-MacBook-Air.local>
2025-08-06 16:07:34 +0530

    setup toml and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Divy Yadav',
+    author_email='yadavdipu296@gmail.com',
+    packages=find_packages(),
+)
+
