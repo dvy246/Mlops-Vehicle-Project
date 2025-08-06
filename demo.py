93c4be7cdbb24e34d3f9824994455c32104d7534
Divy <bhawnayadav@Bhawnas-MacBook-Air.local>
2025-08-06 17:29:35 +0530

    added exception handling

--- demo.py
+++ demo.py
@@ -1,4 +1,15 @@
 from src.logger import logging
+from src.exception import MyException
+import os,sys
+
+
 
 
 logging.info("This is an info message from demo.py")
+
+
+try:
+    a=1+'a'
+except Exception as e:
+    logging.error("An error occurred in demo.py")
+    raise MyException(e, sys) from e
