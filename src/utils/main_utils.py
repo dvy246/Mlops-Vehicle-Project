73e8414c26cf1d6103e5ece9bb0ede33c406da0a
Divy <bhawnayadav@Bhawnas-MacBook-Air.local>
2025-08-09 01:20:42 +0530

    starting-data-validation

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -1,105 +1,138 @@
-import os
 import sys
-
-import numpy as np
-import dill
-import yaml
-from pandas import DataFrame
+import os
 
 from src.exception import MyException
 from src.logger import logging
 
+from pandas import DataFrame
+import numpy as np
+import yaml
+import dill
+
 
 def read_yaml_file(file_path: str) -> dict:
-    try:
-        with open(file_path, "rb") as yaml_file:
-            return yaml.safe_load(yaml_file)
+    logging.info(f"Reading YAML file from {file_path}...")
+    """
+    Reads a YAML file and returns its content as a dictionary.
 
+    Parameters:
+    ----------
+    file_path : str
+        Path to the YAML file.
+
+    Returns:
+    -------
+    dict
+        Content of the YAML file.
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            content = yaml.safe_load(file)
+        return content
     except Exception as e:
-        raise MyException(e, sys) from e
+        raise MyException(e, sys)
+    
+def write_yaml_file(file_path: str, content:object,replace:bool=False) -> None:
+    logging.info(f"Writing content to {file_path}")
+    """
+    Writes a dictionary to a YAML file.
 
-
-def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    Parameters:
+    ----------
+    file_path : str
+        Path to the YAML file.
+    data : dict
+        Dictionary to be written to the YAML file.
+    """
     try:
-        if replace:
-            if os.path.exists(file_path):
-                os.remove(file_path)
+        if replace and os.path.exists(file_path):
+            os.remove(file_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "w") as file:
+
+        with open(file_path, 'wb') as file:
             yaml.dump(content, file)
     except Exception as e:
-        raise MyException(e, sys) from e
-
-
-def load_object(file_path: str) -> object:
+        raise MyException(e, sys)
+    
+def save_object(file_path: str, obj: object) -> None:
+    logging.info(f"Saving object to {file_path}")
     """
-    Returns model/object from project directory.
-    file_path: str location of file to load
-    return: Model/Obj
+    Saves an object to a file using dill.
+
+    Parameters:
+    ----------
+    file_path : str
+        Path to the file where the object will be saved.
+    obj : object
+        Object to be saved.
     """
     try:
-        with open(file_path, "rb") as file_obj:
-            obj = dill.load(file_obj)
+        with open(file_path, 'wb') as file:
+            dill.dump(obj, file)
+    except Exception as e:
+        raise MyException(e, sys)
+    
+def load_object(file_path: str) -> object:
+    logging.info(f"Loading object from {file_path}")
+    """
+    Loads an object from a file using dill.
+
+    Parameters:
+    ----------
+    file_path : str
+        Path to the file from where the object will be loaded.
+
+    Returns:
+    -------
+    object
+        Loaded object.
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            obj = dill.load(file)
         return obj
     except Exception as e:
-        raise MyException(e, sys) from e
-
-def save_numpy_array_data(file_path: str, array: np.array):
+        raise MyException(e, sys)
+    
+def save_numpy_array_data(file_path: str, array: np.ndarray) -> None:
+    logging.info(f"Saving numpy array to {file_path}")
     """
-    Save numpy array data to file
-    file_path: str location of file to save
-    array: np.array data to save
-    """
-    try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, 'wb') as file_obj:
-            np.save(file_obj, array)
-    except Exception as e:
-        raise MyException(e, sys) from e
+    Saves a numpy array to a file.
 
-
-def load_numpy_array_data(file_path: str) -> np.array:
-    """
-    load numpy array data from file
-    file_path: str location of file to load
-    return: np.array data loaded
+    Parameters:
+    ----------
+    file_path : str
+        Path to the file where the numpy array will be saved.
+    array : np.ndarray
+        Numpy array to be saved.
     """
     try:
-        with open(file_path, 'rb') as file_obj:
-            return np.load(file_obj)
-    except Exception as e:
-        raise MyException(e, sys) from e
-
-
-def save_object(file_path: str, obj: object) -> None:
-    logging.info("Entered the save_object method of utils")
-
-    try:
+        os.path.dirname(file_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
-
-        logging.info("Exited the save_object method of utils")
-
+        with open(file_path, 'wb') as file:
+            np.save(file, array)
     except Exception as e:
-        raise MyException(e, sys) from e
+        raise MyException(e, sys)
+    
+def load_numpy_array_data(file_path: str) -> np.ndarray:
+    logging.info(f"Loading numpy array from {file_path}")
+    """
+    Loads a numpy array from a file.
 
+    Parameters:
+    ----------
+    file_path : str
+        Path to the file from where the numpy array will be loaded.
 
-# def drop_columns(df: DataFrame, cols: list)-> DataFrame:
-
-#     """
-#     drop the columns form a pandas DataFrame
-#     df: pandas DataFrame
-#     cols: list of columns to be dropped
-#     """
-#     logging.info("Entered drop_columns methon of utils")
-
-#     try:
-#         df = df.drop(columns=cols, axis=1)
-
-#         logging.info("Exited the drop_columns method of utils")
-        
-#         return df
-#     except Exception as e:
-#         raise MyException(e, sys) from e
+    Returns:
+    -------
+    np.ndarray
+        Loaded numpy array.
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            array = np.load(file)
+        return array
+    except Exception as e:
+        raise MyException(e, sys)
+    
